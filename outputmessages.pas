b22fbc530fb28adcfb763c56854c2dbd4c8862aa
Konrad Kluczewski <kompustelnik92@gmail.com>
2024-05-19 23:42:00 +0200

    Added feature: Added messages window.

---
+++ outputmessages.pas
@@ -0,0 +1,30 @@
+unit OutputMessages;
+
+{$mode Delphi}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm3 }
+
+  TForm3 = class(TForm)
+    lbOutput: TListBox;
+  private
+
+  public
+
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
